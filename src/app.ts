54f145e8958e304ec6939813d46c187ed41ce88d
aslanchik <aslanchikgw2@hotmail.com>
2020-11-10 16:45:22 +0200

    implemented validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+// Define A validation interface with validation rules
+interface Validatable{
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+// Define a validate function that checks each one of the validation interface rules
+function validate(validatableInput: Validatable){
+    // Set initial value to true
+    let isValid = true;
+    // Check if each one of the Validatable interface rules are met and then set isValid to be false if they are 
+    if(validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if(validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if(validatableInput.maxLength !=null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if(validatableInput.min != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    } 
+    if(validatableInput.max != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    // Return true if everything is valid and return false if atleast one of the if checks pinged a validation rule
+    return isValid
+}
 // A Decorator that automatically binds 'this' to the method when it is called
 function autoBind(_: any, _2:string, descriptor:PropertyDescriptor){
     // Get the original method inside the class
@@ -52,8 +84,25 @@ class ProjectInput {
         const titleVal = this.titleInputEl.value;
         const descriptionVal = this.descriptionInputEl.value;
         const peopleVal = this.peopleInputEl.value;
-
-        if(titleVal.trim().length === 0 || descriptionVal.trim().length === 0 || peopleVal.trim().length === 0 ){
+        // Create validatable objects with validation rules
+        const titleValidatable: Validatable = {
+            value: titleVal,
+            required:true,
+            minLength: 5
+        }
+        const descriptionValidatable: Validatable = {
+            value: descriptionVal,
+            required:true,
+            minLength: 20
+        }
+        const peopleValidatable: Validatable = {
+            value: +peopleVal,
+            required:true,
+            min:1,
+            max: 5
+        }
+        // If atleast one input validation fails, return function and alert 
+        if(!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)){
             alert('Invalid input, try again!')
             return;
         } else return [titleVal, descriptionVal, +peopleVal]
